2f636d518420865363222559683735b0e06bd4fb
yukiboard <yukiya062582@gmail.com>
2020-03-14 13:44:57 +0900

    tmp

--- flutter_app_sample/lib/main.dart
+++ flutter_app_sample/lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+
   @override
   Widget build(BuildContext context) => MaterialApp(
     title: 'Flutter Demo',
@@ -19,5 +22,43 @@ class MyApp extends StatelessWidget {
       )
     ),
   );
+}
 
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home:Scaffold(
+      body:Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Incliment',
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+      ),
+    ),
+  );
+
+  void _incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
 }
